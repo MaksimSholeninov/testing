5bbfd72b1bea071b8b4f6b8b1bda24e363b8b39c
MaksimSholeninov <maksym.sholeninov@gmail.com>
2024-07-07 23:07:29 +0200

    сделал

--- making_file.sh
+++ making_file.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+nameDir="$1"
 
-if [  -d "/home/user12345/work/9/testing"  ]; then 
+if [  -d "$nameDir"  ]; then 
      echo "папка существует"
 else 
-     mkdir testing
+     mkdir "$nameDir"
 fi
 
